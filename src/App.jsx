098334bfda81c61e82d7e5288284f589ab7678ff
Mishaj__k <amstig100@gmail.com>
2024-05-06 19:24:51 +0530

    created esential componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import React, { Component } from 'react'
+import ThemeContextProvider from './context/ThemeContext'
+import { Navigation } from './components/Navigation'
+import BookList from './components/BookList'
 
-
-function App() {
-  
-
-  return (
-    <>
-      <h1>hai</h1>
-    </>
-  )
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <ThemeContextProvider>
+          <Navigation />
+          <BookList />
+        </ThemeContextProvider>
+        
+      </>
+    )
+  }
 }
 
-export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        Navigation
+      </div>
+    )
+  }
+}
+
